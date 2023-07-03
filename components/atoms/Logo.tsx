fce7ba9e7ef1bc008d4415f7037e6018592deab8
Арсен Полівка <Arsen.Polivka@lnu.edu.ua>
2023-07-03 15:50:16 +0300

    [#3]: atoms templates for header component created

---
+++ components/atoms/Logo.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Link from "next/link";
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <Image
+        src='/images/logo.png'
+        alt='SpaceX logo'
+        width={229}
+        height={42}
+      />
+    </Link>
+  )
+}
