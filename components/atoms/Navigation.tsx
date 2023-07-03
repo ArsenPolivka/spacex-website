a6763828f09105f6b4c1fcc9b941f09f50605474
Арсен Полівка <Arsen.Polivka@lnu.edu.ua>
2023-07-03 15:55:53 +0300

    [refactor]: code refactored

--- components/atoms/Navigation.tsx
+++ components/atoms/Navigation.tsx
@@ -1,5 +1,12 @@
+import { NavButton } from "./NavButton";
+
 export const Navigation = () => {
   return (
-    <></>
+    <>
+      <NavButton />
+      <NavButton />
+      <NavButton />
+      <NavButton />
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import HomePage from "../components/organisms/HomePage";
 
 export default function Home() {
