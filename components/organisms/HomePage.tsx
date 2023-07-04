b77f3914fd0460c9c05948e6841390ee0e0fbab4
Арсен Полівка <Arsen.Polivka@lnu.edu.ua>
2023-07-04 18:56:39 +0300

    [tech]: code refactored

--- components/organisms/HomePage.tsx
+++ components/organisms/HomePage.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import styled from "styled-components";
 
 import { Header } from "../molecules/Header";
 import { Slider } from "../molecules/Slider";
-
-const StyledWrapper = styled.div`
-  
-`;
-
-const StyledSlider = styled(Slider)`
-  position: absolute;
-`;
 
 const HomePage = () => {
   return (
