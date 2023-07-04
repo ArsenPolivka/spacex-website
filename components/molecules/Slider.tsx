b6e0f3ddb1cc9263641d8a5c3fe3dac5f247d53b
Арсен Полівка <Arsen.Polivka@lnu.edu.ua>
2023-07-04 13:43:59 +0300

    [#4]: molecule created

---
+++ components/molecules/Slider.tsx
@@ -0,0 +1,13 @@
+import { SwitcherButton } from "../atoms/SwitcherButton";
+import { ArrowButton } from "../atoms/ArrowButton";
+
+export const Slider = () => {
+  return (
+    <>
+      <span>The space is waiting for</span>
+      <span>You</span>
+      <SwitcherButton />
+      <ArrowButton />
+    </>
+  )
+}
