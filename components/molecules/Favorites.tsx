f6f0bc230a4d251b9d4f0556b9f04cf1167f8376
Арсен Полівка <Arsen.Polivka@lnu.edu.ua>
2023-07-05 17:28:34 +0300

    [#30]: favorite block created, add logic implemented

--- components/molecules/Favorites.tsx
+++ components/molecules/Favorites.tsx
@@ -1,7 +1,36 @@
-export const Favorites = () => {
-  return (
-    <>
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
 
-    </>
+import { favoriteToursState } from "../../recoil/atoms";
+
+import { Card } from "../atoms/Card";
+
+const StyledContainer = styled.div`
+  padding: 80px;
+`;
+
+const StyledCardList = styled.div`
+  display: flex;
+  gap: 24px;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+export const Favorites = () => {
+  const [favoriteTours] = useRecoilState(favoriteToursState);
+  const images = ["/images/card1.png", "/images/card2.png", "/images/card3.png"];
+
+  return (
+    <StyledContainer>
+      <StyledCardList>
+        {favoriteTours.map((favorite, index) => (
+          <Card
+            rocket={favorite}
+            key={favorite.id}
+            img={images[index % images.length]}
+          />
+        ))}
+      </StyledCardList>
+    </StyledContainer>
   )
 }
